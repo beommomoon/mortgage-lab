b0fc84ff96fb6c3e1e60912f32d8697ff6887f96
your-name <your-email@example.com>
2025-06-19 15:20:39 +0900

    robots.txt에 /admin 제외 설정 추가

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://mortgage-lab.co.kr', // ← 여기가 꼭 있어야 해요!
+  siteUrl: 'https://mortgage-lab.co.kr',
   generateRobotsTxt: true,
+  outDir: './public',
+  exclude: ['/api/*'],
 };
